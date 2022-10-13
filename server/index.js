60a6211226fd32c4871a4f38c41541b00c0f649d
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-13 10:56:23 +0200

    e

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const userModel = require("./models");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 
